d2131edae50a87de4465e67e4422883952196291
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-06-26 15:05:36 +0100

    update institution page

--- components/Institutionpage/TextContent.js
+++ components/Institutionpage/TextContent.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-
+import { LoginModalContext } from '../Context/LoginModalContext'
 
 export default class TextContent extends Component {
   render() {
@@ -13,7 +13,7 @@ export default class TextContent extends Component {
               Proin id arcu odio. Proin tincidunt ante lacus, at commodo felis aliquam eu. Praesent ut turpis venenatis, aliquam mauris tempus, lobortis nunc. Pellentesque maximus ipsum turpis, sed blandit diam placerat ac. Proin in mi ut nulla lacinia facilisis. Nullam semper sapien eget viverra iaculis. Integer quis vestibulum lacus, imperdiet pellentesque libero.
               Cras feugiat dui augue, in malesuada sem porta id. Mauris tempor aliquam risus eget sagittis. Nulla eu imperdiet mauris. Morbi ornare ac elit luctus vestibulum.
               Proin id arcu odio. Proin tincidunt ante lacus, at commodo felis aliquam eu. Praesent ut turpis venenatis, aliquam mauris tempus, lobortis nunc. Pellentesque maximus ipsum turpis, sed blandit diam placerat ac. Proin in mi ut nulla lacinia facilisis. Nullam semper sapien eget viverra iaculis. Integer quis vestibulum lacus, imperdiet pellentesque libero.
-              Cras feugiat dui augue, in malesuada sem porta id. Mauris tempor aliquam risus eget sagittis. Nulla eu imperdiet mauris. Morbi ornare ac elit luctus vestibulum. 
+              Cras feugiat dui augue, in malesuada sem porta id. Mauris tempor aliquam risus eget sagittis. Nulla eu imperdiet mauris. Morbi ornare ac elit luctus vestibulum.
             </p>
           </div>
         </div>
@@ -23,19 +23,30 @@ export default class TextContent extends Component {
             <div className="w3l_services_grids">
               <div className="row">
                 <div className="col-md-7">
+                  <LoginModalContext.Consumer>{
+                    ({handleModalOpen, handleTabChange, changeRegisterType}) => {
+                      const showRegisterModal = (event) => {
+                        event.preventDefault()
+                        event.stopPropagation()
+                        handleModalOpen();
+                        handleTabChange(1);
+                        changeRegisterType(null,null,2);
+                      }
+                      return (<p className="institution">
+                        Institutions may register, to create an institutional account that enables the institution to purchase in bulk prices which are 20 percent
+                        cheaper than individual student purchase.<a onClick={showRegisterModal} href=""> Register as an Institution.</a>
+                      </p>)
+                    }
+                  }</LoginModalContext.Consumer>
                 <p className="institution">
-                   Institutions may register, to create an institutional account that enables the institution to purchase in bulk prices which are 20 percent
-                  cheaper than individual student purchase.<a href=""> Register as an Institution.</a> 
-                </p>
-                <p className="institution">
-                    All institutions that has benefited from MCC program have also recorded a higher level of student performance in their chosen subjects. 
+                    All institutions that has benefited from MCC program have also recorded a higher level of student performance in their chosen subjects.
                 </p>
                 </div>
                 <div className="col-md-5">
                   <img src="/static/images/institution.jpg" style={{width: '100%'}} />
                 </div>
               </div>
-            </div>        
+            </div>
         </div>
       </div>
         <style jsx>{`
