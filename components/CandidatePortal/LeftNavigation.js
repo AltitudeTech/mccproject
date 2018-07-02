08d7454648ca57a3a52dae64ac4eb33304b514f0
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-07-02 17:41:17 +0100

    make user portal functional

---
+++ components/CandidatePortal/LeftNavigation.js
@@ -0,0 +1,127 @@
+import React, {Component, Fragment} from 'react';
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { ApolloConsumer } from 'react-apollo'
+import cookie from 'cookie'
+import redirect from '../../lib/auth/redirect'
+
+import { CandidateDetailsContext } from '../Context/CandidateDetailsContext'
+
+export default class LeftNavigation extends Component{
+  constructor(props){
+    super(props)
+    this.setActive = this.setActive.bind(this)
+  }
+
+  setActive = (e) => {
+    console.log('Free pizza!')
+    console.log(e.target.className)
+  }
+
+    render(){
+        return<div>
+        <div className="profile-sidebar">
+            <div className="profile-usertitle">
+                <img src="http://www.mycareerchoice.global/static/images/mcclogo-text-dark.png" width="100px"/>
+                <style jsx>{`
+                  img: {
+                    border-radius: 50%;
+                    height: 100px;
+                    border: 4px solid #136458;
+                    padding: 10px;
+                    background-color: #f1f3fa;
+                  }
+                `}</style>
+                <CandidateDetailsContext.Consumer>{
+                  ({ candidate: { name } }) => <Fragment>
+                    <div className="profile-usertitle-name">
+                      {name}
+                    </div>
+                  </Fragment>
+                }</CandidateDetailsContext.Consumer>
+                <div className="profile-usertitle-job">
+                    Candidate
+                </div>
+            </div>
+
+            <div className="profile-userbuttons">
+                <button type="button" className="btn btn-danger btn-sm">Message</button>
+            </div>
+
+            <div className="profile-usermenu">
+                <ul className="nav">
+                    <li className={this.props.activePage=='/' ? "active" : ""} >
+                      <Link prefetch href="/user/dashboard">
+                        <a>
+                        <i className="glyphicon glyphicon-home"></i>
+                        Home </a>
+                      </Link>
+                    </li>
+                    <li className={this.props.activePage=='/payment' ? "active" : ""}>
+                        <Link prefetch href="/user/payment">
+                        <a>
+                        <i className="glyphicon glyphicon-user"></i>
+                        Make Payment  </a>
+                        </Link>
+
+                    </li>
+                    <li className={this.props.activePage=='/test' ? "active" : ""}>
+                        <Link prefetch href="/user/mcc-test-code">
+                        <a>
+                        <i className="glyphicon glyphicon-edit"></i>
+                        MCC Test Code </a>
+                        </Link>
+                    </li>
+                    <li className={this.props.activePage=='/career-coach' ? "active" : ""}>
+                        <Link prefetch href="/user/career-coach">
+                        <a>
+                        <i className="glyphicon glyphicon-ok"></i>
+                        Career Coach</a>
+                        </Link>
+                    </li>
+                    <li>
+                      <Link href="/" >
+                        <a target="_blank">
+                          <i className="glyphicon glyphicon-ok"></i>Link To Website
+                        </a>
+                      </Link>
+                    </li>
+                    <li className={this.props.activePage=='/faq' ? "active" : ""}>
+                        <Link prefetch href="/user/faq">
+                        <a>
+                        <i className="glyphicon glyphicon-flag"></i>
+                        Help / FAQ </a>
+                        </Link>
+                    </li>
+                    <li>
+                      <ApolloConsumer>{
+                        client => <a href="#!" onClick={(e)=>{
+                          e.preventDefault();
+                          e.stopPropagation();
+                          document.cookie = cookie.serialize('token', '', {
+                            maxAge: -1 // Expire the cookie immediately
+                          })
+                          document.cookie = cookie.serialize('userType', '', {
+                            maxAge: -1 // Expire the cookie immediately
+                          })
+
+                          // Force a reload of all the current queries now that the user is
+                          // logged in, so we don't accidentally leave any state around.
+                          client.cache.reset().then(() => {
+                            // Redirect to a more useful page when signed out
+                            redirect({}, '/')
+                          })
+                        }}>
+                        <i className="glyphicon glyphicon-log-out"></i>
+                        Logout </a>
+                      }</ApolloConsumer>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+        </div>
+    }
+}
--- pages/user/mcc-test-code.js
+++ pages/user/mcc-test-code.js
@@ -1,64 +1,23 @@
 import React, {Component} from 'react';
-import Head from 'next/head'
-import Link from 'next/link'
-import LeftNavigation from './leftnavigation'
+// import Link from 'next/link'
 import withCandidatePortal from '../../components/HOCS/candidate/withCandidatePortal'
 
 class MccTestCode extends Component{
+    constructor(props){
+        super(props)
+    }
     render(){
-        return<div>
-        <Head>
-          <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-          <meta charSet="utf-8"/> 
-          <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
-          <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-          <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        </Head>
-        <div className="container">
-            <div className="row profile">
-                <div className="col-md-3">
-                    <LeftNavigation test='test' />
-                </div>
-                <div className="col-md-9">
-                    <div className="profile-content">
-                    <div>
-                        <h2>MCC Test Code</h2><hr/>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                        </p>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                        </p>
-                    </div>
-                            
-                    </div>
-                </div>
-            </div>
-        </div>
-        <style jsx>{`
-            body {
-                background: #F1F3FA;
-              }
-              
-              .profile {
-                margin: 10px 0;
-              }
-              
-              .profile-content{box-shadow:1px 1px 10px rgba(0,0,0,0.3);}
-              .profile-content {
-                padding: 20px;
-                background: #f6f6f6;
-                min-height: 600px;
-                margin-bottom:30px;
-              }
-              .profile-content p{
-                font-size : 16px ;
-                line-height : 30px ;
-              }
-        `}
-            </style>
+        return <div className="profile-content">
+          <div>
+              <h2>MCC Test Code</h2><hr/>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              </p>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              </p>
+          </div>
         </div>
     }
 }
-export default withCandidatePortal(MccTestCode)
+export default withCandidatePortal(MccTestCode, {activePage: '/test'})
