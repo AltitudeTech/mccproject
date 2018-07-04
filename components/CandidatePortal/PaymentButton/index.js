c181a7eb6f85e6dbc80eb1af63f9100860372386
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-07-04 10:54:48 +0100

    Make payments with paystack

---
+++ components/CandidatePortal/PaymentButton/index.js
@@ -0,0 +1,89 @@
+import { Component } from 'react';
+import PaystackButton from 'react-paystack';
+import { Mutation } from 'react-apollo';
+
+import { CREATE_PAYMENT_MUTATION } from '../../../lib/backendApi/mutations'
+import { PAYMENT_PAGE_QUERY } from '../../../lib/backendApi/queries'
+
+const PAYSTACK_PUBLIC_KEY = 'pk_test_43f8936830aea3a5b9838c4893b16c9f1e7dee34';
+
+export default class PaymentButton extends Component {
+
+	state = {
+		key: PAYSTACK_PUBLIC_KEY, //PAYSTACK PUBLIC KEY
+		email: this.props.email,  // customer email
+		amount: this.props.amount //equals NGN100,
+	}
+
+	callback = (response, runMutation) => {
+		console.log(response); // card charged successfully, get reference here
+		if (response.reference) {//successfully
+			runMutation({
+				variables: {
+					paystackReference: response.reference
+				}
+			})
+		}
+	}
+
+	close = () => {
+		console.log("Payment closed");
+	}
+
+	getReference = () => {
+		//you can put any unique reference implementation code here
+		let text = "";
+		let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.=";
+
+		for( let i=0; i < 15; i++ )
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+		return text;
+	}
+
+	onMutationCompleted = (cache, { data: { candidateFindOrCreatePaymentRecord } }) => {
+		// Read the data from our cache for this query.
+		const data = cache.readQuery({ query: PAYMENT_PAGE_QUERY });
+
+		// Add the new education to PAYMENT_PAGE_QUERY
+		data.viewerCandidate.candidate.payments = [
+			candidateFindOrCreatePaymentRecord,
+			...data.viewerCandidate.candidate.payments
+		];
+
+		// Write our data back to the cache.
+		cache.writeQuery({ query: PAYMENT_PAGE_QUERY, data });
+		//add payment to top of candidate payments
+	}
+
+  render() {
+    return (
+      <div>
+        <script src="https://js.paystack.co/v1/inline.js"></script>
+				<Mutation mutation={CREATE_PAYMENT_MUTATION}
+					update={this.onMutationCompleted}
+					onError={(error) => {
+          	console.log(error)
+        	}}>
+					{(candidateFindOrCreatePaymentRecord, {data, error}) => (
+						<p>
+							{error && <p>:( There was an issue check the logs</p>}
+							<PaystackButton
+								text="Pay now"
+								class="payButton"
+								callback={(response)=>{this.callback(response, candidateFindOrCreatePaymentRecord)}}
+								close={this.close}
+								disabled={false}
+								embed={false}
+								reference={this.getReference()}
+								email={this.state.email}
+								amount={this.state.amount}
+								paystackkey={this.state.key}
+							/>
+						</p>
+					)}
+				</Mutation>
+      </div>
+    );
+  }
+}
--- pages/user/payment.js
+++ pages/user/payment.js
@@ -1,6 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 // import Link from 'next/link'
 import withCandidatePortal from '../../components/HOCS/candidate/withCandidatePortal'
+
+import PaymentButton from '../../components/CandidatePortal/PaymentButton'
+
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import SelectField from 'material-ui/SelectField';
@@ -21,6 +24,8 @@ class PaymentPage extends Component{
 
         this.state = {
           value: 10,
+          email: 'bellooladipupo41@gmail.com',
+          amount: 100*100 // equals 100NGN
         };
     }
 
@@ -30,34 +35,37 @@ class PaymentPage extends Component{
     };
     render(){
         return <div className="profile-content">
-          <div>
+          <CandidatePaymentsWrapper>
+            <div>
               <h2>Payment Portal</h2><hr/>
               <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
               </p>
-          </div>
-          <div className="col-md-6">
-          <Card>
-              <CardTitle title="MCC Test Code" subtitle="Career Intelligence" />
-              <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Donec mattis pretium massa. Aliquam erat volutpat.
-              </CardText>
-              <CardActions>
-              <SelectField
-                  floatingLabelText="Select Currency"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                  maxHeight={200}
-                  style={styles.customWidth}
-              >
-                  <MenuItem value={null} primaryText="" />
-                  <MenuItem value={"USD"} primaryText="USD" />
-                  <MenuItem value={"POUNDS"} primaryText="POUNDS" />
-                  <MenuItem value={"NAIRA"} primaryText="NAIRA" />
-              </SelectField>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <Card>
+                  <CardTitle title="MCC Test Code" subtitle="Career Intelligence" />
+                  <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec mattis pretium massa. Aliquam erat volutpat.
+                  </CardText>
+                  <CardActions>
+                    <SelectField
+                      floatingLabelText="Select Currency"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                      maxHeight={200}
+                      style={styles.customWidth}
+                      >
+                        <MenuItem value={null} primaryText="" />
+                        <MenuItem value={"USD"} primaryText="USD" />
+                        <MenuItem value={"POUNDS"} primaryText="POUNDS" />
+                        <MenuItem value={"NAIRA"} primaryText="NAIRA" />
+                      </SelectField>
 
-              <RaisedButton primary={true} label="Pay NOW" />
+                      <RaisedButton primary={true} label="Pay NOW" />
+                      <PaymentButton email={this.state.email} amount={this.state.amount}/>
 
                   </CardActions>
                 </Card>
