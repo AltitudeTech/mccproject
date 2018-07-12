48381ec406497ca71199ca19dbd3a1dc7dd83f0c
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-07-12 12:58:31 +0100

    add local bootstrap

--- components/HOCS/candidate/withCandidatePortal.js
+++ components/HOCS/candidate/withCandidatePortal.js
@@ -65,7 +65,7 @@ export default function withLayout(Child, opts={}) {
             {/* <!-- Stylesheets--> */}
             {/* <link rel="stylesheet" href="/static/css/portal/style.css"/> */}
             {/* <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/> */}
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+            <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
             <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
             <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
           </Head>
